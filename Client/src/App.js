23877c747128255121866aa6f07adb9c1ade1959
harshitjain156 <92665993+harshitjain156@users.noreply.github.com>
2022-02-22 12:16:12 +0530

    star

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,18 @@
-
 import './App.css';
+import Mainfront from './components/Mainfront';
+
+
 import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <>
     <div className="App">
       <Navbar />
     </div>
-  );
+      <Mainfront />
+    </>
+      );
 }
 
 export default App;
---
+++ Client/src/components/Mainfront.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Mainfront() {
+  return (
+    <div className='bg-red-700  w-1/2 h-96 '>
+        
+        {/* <div className="card" style="width: 18rem;">
+  <img src="" className="card-img-top" alt="" />
+  <div className="card-body">
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div> */}
+
+    </div>
+  )
+}
+
+export default Mainfront;
