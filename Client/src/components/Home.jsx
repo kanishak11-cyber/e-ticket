4902e4977da82bcbfecccc9e97c6fe4865e6ece5
harshitjain156 <92665993+harshitjain156@users.noreply.github.com>
2022-02-23 12:05:36 +0530

    image text & set on card

--- Client/src/components/Home.jsx
+++ Client/src/components/Home.jsx
@@ -1,45 +1,46 @@
 import React from 'react'
+import './Home.css';
 
 const Home = () => {
   return (
     <div>
       <div className='justify-evenly mx-20 my-10  flex flex-row flex-1'>
-        <div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
+        <div className=" state_image w-64 h-40 white-glassmorphism  rounded-xl" id='photo'>
           <div>
-          <h1 className='p-2'>uttar pradesh</h1>
+          <h1 className='state_name'>uttar pradesh</h1>
           </div>
         </div>
         <div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
           <div>
-          <h1 className='p-2'>uttar pradesh</h1>
+          <h1 className='state_name'>uttar pradesh</h1>
           </div>
         </div><div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
           <div>
-          <h1 className='p-2'>uttar pradesh</h1>
+          <h1 className='state_name'>uttar pradesh</h1>
           </div>
         </div><div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
           <div>
-          <h1 className='p-2'>uttar pradesh</h1>
+          <h1 className='state_name'>uttar pradesh</h1>
           </div>
         </div>
       </div>
       <div className='justify-evenly mx-20 my-10  flex flex-row flex-1'>
         <div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
           <div>
-          <h1 className='p-2'>uttar pradesh</h1>
+          <h1 className='state_name'>uttar pradesh</h1>
           </div>
         </div>
         <div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
           <div>
-          <h1 className='p-2'>uttar pradesh</h1>
+          <h1 className='state_name'>uttar pradesh</h1>
           </div>
         </div><div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
           <div>
-          <h1 className='p-2'>uttar pradesh</h1>
+          <h1 className='state_name'>uttar pradesh</h1>
           </div>
         </div><div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
           <div>
-          <h1 className='p-2'>uttar pradesh</h1>
+          <h1 className='state_name'>uttar pradesh</h1>
           </div>
         </div>
       </div>
