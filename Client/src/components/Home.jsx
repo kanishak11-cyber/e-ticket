643799418e42370ac42541cf953a911d5324a3b7
Kanishak Chaurasia <kanishakchaurasia2@gmail.com>
2022-02-23 12:47:29 +0530

    map

--- Client/src/components/Home.jsx
+++ Client/src/components/Home.jsx
@@ -1,103 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+
+  // const [stateData, setStateData] = useState([]);
+  
+  // const getStates = async () => {
+  //   const response = await fetch("https://indstatesapi.anandpy2156.repl.co/api");
+    
+  //   console.log(response);
+  //   setStateData(await response.json());
+  //   console.log(data);
+  // };
+  // useEffect(() => {
+  //   getStates();
+  // }, []);
+
   return (
     <div>
       <div className='justify-evenly mx-20 my-10  flex flex-row flex-1'>
         <div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div>
-        <div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div><div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div><div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div>
-      </div>
-      <div className='justify-evenly mx-20 my-10  flex flex-row flex-1'>
-        <div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div>
-        <div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div><div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div><div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div>
-      </div>
-      <div className='justify-evenly mx-20 my-10  flex flex-row flex-1'>
-        <div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div>
-        <div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div><div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div><div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div>
-      </div>
-      <div className='justify-evenly mx-20 my-10  flex flex-row flex-1'>
-        <div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div>
-        <div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div><div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div><div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div>
-      </div>
-      <div className='justify-evenly mx-20 my-10  flex flex-row flex-1'>
-        <div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div>
-        <div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div><div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
-          <div>
-          <h1 className='p-2'>uttar pradesh</h1>
-          </div>
-        </div><div className='w-64 h-40 white-glassmorphism bg-gradient-to-r from-blue-900 to-blue-400 rounded-xl'>
           <div>
           <h1 className='p-2'>uttar pradesh</h1>
           </div>
