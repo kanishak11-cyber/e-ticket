448e6b806a00170ffb73acfa3cd2553a665fce47
harshitjain156 <92665993+harshitjain156@users.noreply.github.com>
2022-03-09 23:52:15 +0530

    Cards Added

--- src/components/StateCard.jsx
+++ src/components/StateCard.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
+import './Card.css';
+import he from '../components/he.jpg';
 
-const StateCard = () => {
-  return (
-    <div>StateCard</div>
-  )
+export class StateCard extends Component {
+  render() {
+   
+    return (
+      <div>
+         <div className="box">
+
+            <div className="front ">
+
+                <img src={he} alt='hello' height='250' width='300' />
+
+            </div>
+
+            <div className="back">
+
+                <h1>stateName</h1>
+
+            </div>
+
+            </div>
+      </div>
+    )
+  }
 }
 
 export default StateCard
